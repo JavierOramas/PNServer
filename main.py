29c9660674d0f10db07c14bcef366c716208f62f
Javier Oramas <javiale2000@gmail.com>
2020-08-21 19:55:38 -0400

    favicon added with the software logo index template adedd server up page working

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/debug')
 def debug():
-    return 'Server is Up!'
+    return render_template('server_up.html',title='Debug', content='Server is Up!', paragraph='The Server is Up and running, if you expected someting diferent than this erase cache in your web browser')
 
 @app.route('/')
 def root():
