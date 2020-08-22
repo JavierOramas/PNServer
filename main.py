30fe159953f7e60eeef192a8bbb100c011315489
Javier Oramas <javiale2000@gmail.com>
2020-08-21 20:26:22 -0400

    servicios mostrandose working on the pictures foto emby recortada

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/debug')
+def get_services():
+    return [('emby','static/img/services/emby.png'),('shares','static/img/services/share.png')]
 def debug():
-    return render_template('server_up.html',title='Debug', content='Server is Up!', paragraph='The Server is Up and running, if you expected someting diferent than this erase cache in your web browser')
+    return render_template('server_up.html',title='Debug', content='Server is Up!', paragraph='The Server is Up and running, if you expected someting diferent than this erase cache in your web browser', services=get_services())
 
 @app.route('/')
 def root():
