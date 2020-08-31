b1f491402eba286f1b14b6b71e65f871699bd636
Javier Oramas <javiale2000@gmail.com>
2020-08-31 19:31:07 -0400

    delete entries from DB

--- components/base/content_endpoints.py
+++ components/base/content_endpoints.py
@@ -73,13 +73,14 @@ def scan_network(username='guest'):
 def manage_page_users():
     if request.method == 'POST':
         if request.form['submit-user'] == 'Create':
-            try:
-                ciphered_pwd = generate_password_hash(request.form["password"], method='sha256')
-                new_user = Users(name=request.form["name"], pwd=ciphered_pwd, access_name=request.form['access_name'], access_code=access[request.form['access_name']], user_categories = access.keys())
-                db.session.add(new_user)
-                db.session.commit()
-            except:
-                pass
+            
+            print('here')
+            ciphered_pwd = generate_password_hash(request.form["password"], method='sha256')
+            new_user = Users(name=request.form["name"], pwd=ciphered_pwd, access_name=request.form['access_name'], access_code=access[request.form['access_name']])
+            db.session.add(new_user)
+            db.session.commit()
+            # except:
+                # pass
                 # return render_template('login.html', warning=True)
     return render_template('manage.html', header_title='Manage Users', login='True', user='test', property='users', data=get_data('users'),  user_categories = access.keys())
 
@@ -88,7 +89,7 @@ def manage_page_services():
         if request.form['submit-service'] == 'Create':
             try:
                 # ciphered_pwd = generate_password_hash(request.form["password"], method='sha256')
-                new_service = Services(name=request.form["name"], port=request.form['port'], access_name=request.form['access_name'], access_code=access[request.form['access_name']], user_categories = access.keys())
+                new_service = Services(name=request.form["name"], port=request.form['port'], access_name=request.form['access_name'], access_code=access[request.form['access_name']])
                 db.session.add(new_service)
                 db.session.commit()
             except:
@@ -96,6 +97,14 @@ def manage_page_services():
        
     return render_template('manage.html', header_title='Manage Services', login='True', user='test', property='services', data=get_data('services'),  user_categories = access.keys())
 
+def delete_entry(table, id):
+    if table == 'users':
+        element = Users.query.get(id)
+    if table == 'services':
+        element = Services.query.get(id)
+    db.session.delete(element)
+    db.session.commit()
+    return redirect('/manage')
 ## Tools #######################################################################################################
 
 def loged_user():    
--- main.py
+++ main.py
@@ -5,8 +5,10 @@ from get_ip import get_ip
 from scan import load_services, check_local_services, check_network_machines
 from dbmodel import Users, Properties, Services, app, db
 
-from components.base.content_endpoints import debug, root, logout, login, scan_network, manage_page_users, manage_page_services, loged_user, get_user_access, get_services, get_data, init_db
-from components.temp_monitor.api import get_temps, measure, clean, temp_api
+from components.base.content_endpoints import debug, root, logout, login, scan_network
+from components.base.content_endpoints import manage_page_users, manage_page_services, loged_user
+from components.base.content_endpoints import get_user_access, get_services, get_data, init_db, delete_entry
+# from components.temp_monitor.api import get_temps, measure, clean, temp_api
 
 @app.route('/debug')
 def route_debug():
@@ -44,20 +46,22 @@ def route_manage_users():
 def route_manage_sevices():
     return manage_page_services()
 
-
+@app.route('/delete/<string:table>/<int:id>')
+def route_delete_entry(table, id):
+    return delete_entry(table, id)
 #TODO see how to rediect port 9998 to this route
 ## Temp Monitor 
-@app.route('/temps')
-def route_temps():
-    return get_temps()
+# @app.route('/temps')
+# def route_temps():
+#     return get_temps()
 
-@app.route('/measure')
-def route_measure():
-    return measure()
+# @app.route('/measure')
+# def route_measure():
+#     return measure()
 
-@app.route('/clean')
-def route_temps():
-    return temps()
+# @app.route('/clean')
+# def route_temps():
+#     return temps()
 
 if __name__ == '__main__':
 
